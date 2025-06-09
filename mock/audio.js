8aaf1d1d31166fff8508893dfafaac2195705046
codefengg <1060558028@qq.com>
2025-06-09 22:30:34 +0800

    完善数据

--- mock/audio.js
+++ mock/audio.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         "id": "001",
         "name": "绵绵细雨",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8.png?sign=1ffbb2b3e4647c2acccf9de76a73ac8b&t=1749460457",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8-%E5%B0%8F%E5%9B%BE.png?sign=906b4b1bfdd6d248240f5dab5d291406&t=1749460652",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8.png?sign=e54638ab1fe30587fe0c4e5f2992fef5&t=1749460860",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E4%B8%8B%E9%9B%A8%E5%A4%A9.mp3?sign=911778305f6ebeb0528b7e71a61d2ac8&t=1749458831"
@@ -10,7 +10,7 @@ module.exports = [
     {
         "id": "002",
         "name": "倾盆大雨",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E9%9B%B7%E9%9B%A8.png?sign=3ef15d175b3e324bf1454f44000782b7&t=1749460361",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E9%9B%B7%E9%9B%A8.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E5%80%BE%E7%9B%86%E5%A4%A7%E9%9B%A8-%E5%B0%8F%E5%9B%BE.png?sign=687df5744ec6515006d51f3859234677&t=1749460678",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E5%80%BE%E7%9B%86%E5%A4%A7%E9%9B%A8.png?sign=0bb440267cd086648d78dcafe96d6290&t=1749460887",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E5%A4%A7%E9%9B%A8.mp3?sign=29adc991dc4a33867d082e13434b5035&t=1749458851"
@@ -18,7 +18,7 @@ module.exports = [
     {
         "id": "003",
         "name": "浪花涌动",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E6%B5%AA%E8%8A%B1.png?sign=ecb8690a8b63a93b54ebc46be6ece069&t=1749460504",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E6%B5%AA%E8%8A%B1.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E6%B5%AA%E8%8A%B1%E6%B6%8C%E5%8A%A8-%E5%B0%8F%E5%9B%BE.png?sign=996530831211146cf6d2a2e4c7633d3c&t=1749460691",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E6%B5%AA%E8%8A%B1%E6%B6%8C%E5%8A%A8.png?sign=d35c0bf3f181a11bbf109a21928c4436&t=1749460902",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E6%B5%B7%E6%B5%AA.mp3?sign=7de7d18ca69462e0539e5132f42591a2&t=1749458865"
@@ -26,7 +26,7 @@ module.exports = [
     {
         "id": "004",
         "name": "篝火燃烧",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%AF%9D%E7%81%AB.png?sign=79e6291e7b71876f5d61eb2b0ac935af&t=1749460525",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%AF%9D%E7%81%AB.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%86%8A%E7%86%8A%E7%AF%9D%E7%81%AB-%E5%B0%8F%E5%9B%BE.png?sign=ce60554fbd40f957204e3e3be00119bc&t=1749460712",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%86%8A%E7%86%8A%E7%AF%9D%E7%81%AB.png?sign=bcec8132582d1f37e1f52916d54a4b90&t=1749460918",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E6%9C%A8%E6%9D%90%E7%87%83%E7%83%A7.mp3?sign=43b425b5b362fc281a9e32d237ee4d31&t=1749458874"
@@ -34,7 +34,7 @@ module.exports = [
     {
         "id": "005",
         "name": "稻田乐章",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0.png?sign=590aa3bc0bc7bac8ee4a55bba0c31bf5&t=1749460556",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0-%E5%B0%8F%E5%9B%BE.png?sign=3fdd27207b631bb88ebfef0e1e4c0b38&t=1749460727",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0.png?sign=044b155f5b6c61e8672e4d38110eb634&t=1749460935",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E7%A8%BB%E7%94%B0.mp3?sign=6b9a73bd372a3657b276548f7c06932a&t=1749458885"
@@ -42,7 +42,7 @@ module.exports = [
     {
         "id": "007",
         "name": "风铃撞击",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E9%A3%8E%E9%93%83.png?sign=5b83cfa793c926110fbc3c004d886c44&t=1749460576",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E9%A3%8E%E9%93%83.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E9%A3%8E%E9%93%83-%E5%B0%8F%E5%9B%BE.png?sign=92b71ffdc05a3f4788c7887874b32592&t=1749460741",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E9%A3%8E%E9%93%83.png?sign=5063a7578814d9bfba671de8eb37c84b&t=1749460954",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E9%A3%8E%E9%93%83.mp3?sign=0a4e86db051aaa9d730e519dab63949e&t=1749458895"
@@ -50,7 +50,7 @@ module.exports = [
     {
         "id": "008",
         "name": "睡觉的猫",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%9D%A1%E8%A7%89%E7%8C%AB.png?sign=fe0a296d960c6f5a28a116d14f09f86b&t=1749460590",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%9D%A1%E8%A7%89%E7%8C%AB.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%9D%A1%E8%A7%89%E7%9A%84%E7%8C%AB.png?sign=ce5a2c80289c5d730bf1803c4140cb7d&t=1749460754",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%9D%A1%E8%A7%89%E7%9A%84%E7%8C%AB.png?sign=57dc9604b76a3c40b094f7d8641bb168&t=1749460967",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E7%8C%AB%E7%9D%A1%E8%A7%89.mp3?sign=b5092c55c2c56167249042b1abb18515&t=1749458904"
@@ -58,7 +58,7 @@ module.exports = [
     {
         "id": "009",
         "name": "沉睡的狗",
-        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%9D%A1%E8%A7%89%E7%8B%97.png?sign=db4046cf1161607fe42889c6efad96c7&t=1749460604",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%9D%A1%E8%A7%89%E7%8B%97.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%8B%97-%E5%B0%8F%E5%9B%BE.png?sign=93cb77bcccb55467983f553cff83b960&t=1749460771",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%8B%97.png?sign=31fc2c34f5298a81045c8749515286be&t=1749460979",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E7%8B%97%E7%9D%A1%E8%A7%89.mp3?sign=962a795f0a8f3b0ed3d6ce7b44a7c2ce&t=1749458911"
