7b625523590a6984fa310e64619897b09e62ce6c
v_wangfeng17 <v_wangfeng17@baidu.com>
2025-06-16 11:21:01 +0800

    更换新音频

--- mock/audio.js
+++ mock/audio.js
@@ -5,7 +5,7 @@ module.exports = [
         "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8-%E5%B0%8F%E5%9B%BE.png?sign=906b4b1bfdd6d248240f5dab5d291406&t=1749460652",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%BB%B5%E7%BB%B5%E7%BB%86%E9%9B%A8.png?imageMogr2/blur/26x9",
-        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E4%B8%8B%E9%9B%A8%E5%A4%A9.mp3?sign=911778305f6ebeb0528b7e71a61d2ac8&t=1749458831"
+        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E4%B8%8B%E9%9B%A8-30min.mp3"
     },
     {
         "id": "002",
@@ -13,7 +13,7 @@ module.exports = [
         "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E9%9B%B7%E9%9B%A8.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E5%80%BE%E7%9B%86%E5%A4%A7%E9%9B%A8-%E5%B0%8F%E5%9B%BE.png?sign=687df5744ec6515006d51f3859234677&t=1749460678",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E5%80%BE%E7%9B%86%E5%A4%A7%E9%9B%A8.png?imageMogr2/blur/26x9",
-        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E5%A4%A7%E9%9B%A8.mp3?sign=29adc991dc4a33867d082e13434b5035&t=1749458851"
+        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E9%9B%B7%E9%9B%A8.mp3"
     },
     {
         "id": "003",
@@ -21,7 +21,7 @@ module.exports = [
         "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E6%B5%AA%E8%8A%B1.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E6%B5%AA%E8%8A%B1%E6%B6%8C%E5%8A%A8-%E5%B0%8F%E5%9B%BE.png?sign=996530831211146cf6d2a2e4c7633d3c&t=1749460691",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E6%B5%AA%E8%8A%B1%E6%B6%8C%E5%8A%A8.png?imageMogr2/blur/26x9",
-        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E6%B5%B7%E6%B5%AA.mp3?sign=7de7d18ca69462e0539e5132f42591a2&t=1749458865"
+        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E6%B5%B7%E6%B5%AA%EF%BC%88%E9%95%BF%EF%BC%89.mp3"
     },
     {
         "id": "004",
@@ -37,7 +37,7 @@ module.exports = [
         "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0.png",
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0-%E5%B0%8F%E5%9B%BE.png?sign=3fdd27207b631bb88ebfef0e1e4c0b38&t=1749460727",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%A8%BB%E7%94%B0%E4%B9%90%E7%AB%A0.png?imageMogr2/blur/26x9",
-        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E7%A8%BB%E7%94%B0.mp3?sign=6b9a73bd372a3657b276548f7c06932a&t=1749458885"
+        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E7%A8%BB%E7%94%B0_01.mp3"
     },
     {
         "id": "006",
@@ -70,5 +70,21 @@ module.exports = [
         "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E7%8B%97-%E5%B0%8F%E5%9B%BE.png?sign=93cb77bcccb55467983f553cff83b960&t=1749460771",
         "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E7%8B%97.png?imageMogr2/blur/26x9",
         "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E7%8B%97%E7%9D%A1%E8%A7%89.mp3?sign=962a795f0a8f3b0ed3d6ce7b44a7c2ce&t=1749458911"
+    },
+    {
+        "id": "010",
+        "name": "溪流",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E6%BA%AA%E6%B5%81%20(1).png",
+        "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E6%BA%AA%E6%B5%81.png",
+        "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3D%E6%BA%AA%E6%B5%81.png",
+        "audioUrl": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/audio/%E6%BA%AA%E6%B5%81%E5%A3%B0.mp3"
+    },
+    {
+        "id": "011",
+        "name": "滴水",
+        "icon": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/icons/%E6%BB%B4%E6%B0%B4.png",
+        "listImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/list/%E6%BB%B4%E6%B0%B4%20(1).png",
+        "backgroundImage": "https://636c-cloud1-8glt6bb456600701-1349745487.tcb.qcloud.la/back/Property%201%3DVariant11.png",
+        "audioUrl": ""
     }
 ];
